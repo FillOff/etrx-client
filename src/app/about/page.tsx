57fdf1fee5d2a9909b5f28dca319261c0ae4c859
FillOff <filloff2022@outlook.com>
2025-01-09 18:32:35 +0300

    rework about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,70 +1,94 @@
-
+import Styles from "../components/network-table.module.css";
 
 export default function Page()
 {
     return(
         <>
         <h1 className="m-auto text-center text-2xl md:text-3xl font-bold">Обновления</h1>
-        <div className="ml-8 text-lg">
-            <label>v0.8.0 - 05.01.2025</label>
-            <ul className="border-b-[1px] border-dashed w-[80%] list-disc">
-                <li>Добавлено
-                    <div className="ml-6">
-                        <ul className="list-circle">
-                            <li>Таблица задач</li>
-                            <li>Фильтрация задач по тегам</li>
-                            <li>Фильтрация по типу участия в попытках пользователей</li>
-                        </ul>
-                    </div>
-                </li>
-                <li>Переделано/Изменено
-                    <div className="ml-6">
-                        <ul className="list-circle">
-                            <li>Отображаемые данные в попытках пользователей</li>
-                            <li>Фильтр соревнований/тренировок</li>
-                        </ul>
-                    </div>
-                </li>
-                <li>Исправлено
-                    <div className="ml-6">
-                        <ul className="list-circle">
-                            <li>Данные при выводе всех попыток пользователя были некорректными</li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div className="ml-8 text-lg">
-            <label>v0.7.0 - 08.12.2024</label>
-            <ul className="border-b-[1px] border-dashed w-[80%] list-disc">
-                <li>Добавлено
-                    <div className="ml-6">
-                        <ul className="list-circle">
-                            <li>Страница о сайте, т.е. данная страница</li>
-                        </ul>
-                    </div>
-                </li>
-                <li>Переделано/Изменено
-                    <div className="ml-6">
-                        <ul className="list-circle">
-                            <li>Отображение попыток в таблице контеста</li>
-                            <li>Боковая панель</li>
-                        </ul>
-                    </div>
-                </li>
-                <li>Исправлено
-                    <div className="ml-6">
-                        <ul className="list-circle">
-                            <li>Таблица контестов теперь корректно обновляется при выборе фильтра</li>
-                            <li>Запросы данных контестов</li>
-                            <li>Сортировка таблицы пользователей</li>
-                            <li>Некорректный стиль для фона фильтров у таблицы контестов</li>
-                            <li>Отображение иконок в боковом меню</li>
-                            <li>Размеры и положение кнопки бокового меню</li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
+        <div className={Styles.container}>
+            <table className={Styles.table}>
+                <thead>
+                    <tr>
+                        <th className={Styles.th}>Версия</th>
+                        <th className={Styles.th}>Изменения</th>    
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className={Styles.cell}>
+                            <div className="text-center">5.1.2025</div>
+                        </td>
+                        <td className={Styles.cell}>
+                            <div className="ml-8">
+                                <ul className="w-[80%] list-disc">
+                                    <li>Добавлено
+                                        <div className="ml-6">
+                                            <ul className="list-circle">
+                                                <li>Таблица задач</li>
+                                                <li>Фильтрация задач по тегам</li>
+                                                <li>Фильтрация по типу участия в попытках пользователей</li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li>Переделано/Изменено
+                                        <div className="ml-6">
+                                            <ul className="list-circle">
+                                                <li>Отображаемые данные в попытках пользователей</li>
+                                                <li>Фильтр соревнований/тренировок</li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li>Исправлено
+                                        <div className="ml-6">
+                                            <ul className="list-circle">
+                                                <li>Данные при выводе всех попыток пользователя были некорректными</li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className={Styles.cell}>
+                            <div className="text-center">8.12.2024</div>
+                        </td>
+                        <td className={Styles.cell}>
+                            <div className="ml-8">
+                                <ul className="w-[80%] list-disc">
+                                    <li>Добавлено
+                                        <div className="ml-6">
+                                            <ul className="list-circle">
+                                                <li>Страница о сайте, т.е. данная страница</li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li>Переделано/Изменено
+                                        <div className="ml-6">
+                                            <ul className="list-circle">
+                                                <li>Отображение попыток в таблице контеста</li>
+                                                <li>Боковая панель</li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li>Исправлено
+                                        <div className="ml-6">
+                                            <ul className="list-circle">
+                                                <li>Таблица контестов теперь корректно обновляется при выборе фильтра</li>
+                                                <li>Запросы данных контестов</li>
+                                                <li>Сортировка таблицы пользователей</li>
+                                                <li>Некорректный стиль для фона фильтров у таблицы контестов</li>
+                                                <li>Отображение иконок в боковом меню</li>
+                                                <li>Размеры и положение кнопки бокового меню</li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         </>
     )
