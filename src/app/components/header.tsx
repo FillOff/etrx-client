7e7ceefafd1b371e60d94d0dc6b546f9b78f7e1b
FillOff <filloff2022@outlook.com>
2025-01-09 19:04:38 +0300

    rework version display and rework versioning system

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -12,22 +12,29 @@ export default function Header()
     return(
         <>
             <div className="flex items-center fixed w-full h-16 bg-main">
-                <h1 className={DdStyles.header_elem_static + ' select-none bg-main-light font-bold italic text-[28px]'}>
-                    ETRX
-                </h1>
-                <button onClick={() => router.push('/')} className={DdStyles.header_elem}>
-                    Главная
-                </button>
-                <Dropdown header="Контесты" onClick={() => router.push('/contests/view')}>
-                    <button onClick={() => router.push('/contests/view')} className={DdStyles.dropdown_elem}>Просмотреть все</button>
-                    <button className={DdStyles.dropdown_elem}><div className={MiscStyles.add_ico}></div> Добавить</button>
-                </Dropdown>
-                <Dropdown header="Ученики" onClick={() => router.push('/users/view')}>
-                    <button onClick={() => router.push('/users/view')} className={DdStyles.dropdown_elem}>Просмотреть всех</button>
-                    <button className={DdStyles.dropdown_elem}><div className={MiscStyles.add_ico}></div> Добавить</button>
-                </Dropdown>
-                <button onClick={() => router.push('/problems/view')} className={DdStyles.header_elem}>Задачи</button>
-                <button onClick={() => router.push('/about')} className={DdStyles.header_elem}>О сайте</button>
+                <div className="flex items-center w-fit">
+                    <h1 className={DdStyles.header_elem_static + ' select-none bg-main-light font-bold italic text-[28px]'}>
+                        ETRX
+                    </h1>
+                    <button onClick={() => router.push('/')} className={DdStyles.header_elem}>
+                        Главная
+                    </button>
+                    <Dropdown header="Контесты" onClick={() => router.push('/contests/view')}>
+                        <button onClick={() => router.push('/contests/view')} className={DdStyles.dropdown_elem}>Просмотреть все</button>
+                        <button className={DdStyles.dropdown_elem}><div className={MiscStyles.add_ico}></div> Добавить</button>
+                    </Dropdown>
+                    <Dropdown header="Ученики" onClick={() => router.push('/users/view')}>
+                        <button onClick={() => router.push('/users/view')} className={DdStyles.dropdown_elem}>Просмотреть всех</button>
+                        <button className={DdStyles.dropdown_elem}><div className={MiscStyles.add_ico}></div> Добавить</button>
+                    </Dropdown>
+                    <button onClick={() => router.push('/problems/view')} className={DdStyles.header_elem}>Задачи</button>
+                    <button onClick={() => router.push('/about')} className={DdStyles.header_elem}>О сайте</button>
+
+                </div>
+                <div className="flex flex-col items-left w-fit text-xs ml-auto mr-2">
+                    <div>Version: {process.env.NEXT_PUBLIC_VERSION}</div>
+                    <div>Build: {process.env.NODE_ENV}</div>
+                </div>
             </div>
             {/* acts as bottom margin */}
             <div className="w-full h-20"></div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,8 @@ export default function Home() {
 
     return (
         <>
-            <h1 className="w-vlw text-center font-bold text-3xl">ETRX</h1>
-            <div className="flex flex-col items-center">
-                <div>Last update: {process.env.LAST_UPDATE}</div>
-                <div>Version: v{process.env.VERSION}</div>
-                <div>Build: {process.env.NODE_ENV}</div>
-            </div>
-            <h1 className="w-full text-center font-bold text-3xl mt-10">Последние 10 контестов</h1>
-            <HomeTable/>
+            <h1 className="w-full text-center font-bold text-3xl">Последние 10 контестов</h1>
+            <HomeTable />
         </>
     );
 }
